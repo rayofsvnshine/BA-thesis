84fa57ed51987e28c78812dc7efe7af8200813cb
ray pelupessy <tatum44@outlook.com>
2022-04-24 14:38:49 +0200

    removed model types

--- Full_dRBM/dRBM_network.py
+++ Full_dRBM/dRBM_network.py
@@ -8,12 +8,11 @@ import matplotlib.pyplot as plt
 # Setup the network, load data, select network type, and set the number of trainingsteps
 new_nodes_connections = setup_network()
 new_data = setup_data()
-model_type = "reg"
 training_steps = 1000
 
 # Initiate training
 for _ in range(training_steps):
-    new_nodes_connections[0] = input_word(new_nodes_connections[0], model_type, new_data)
+    new_nodes_connections[0] = input_word(new_nodes_connections[0], new_data)
     new_nodes_connections = one_learning_step(new_nodes_connections[0], new_nodes_connections[1])
 
 # show graph
--- Full_dRBM/setup_input/irish_input.py
+++ Full_dRBM/setup_input/irish_input.py
@@ -19,7 +19,7 @@ def setup_data():
     
     return input_df
 
-def input_word(new_nodes, model_type, full_dataset):
+def input_word(new_nodes, full_dataset):
     """ This function chooses a random input word and changes the 
         activations on the input layer accordingly
         
@@ -38,26 +38,13 @@ def input_word(new_nodes, model_type, full_dataset):
     random_index = random.choice(full_dataset.index)
     random_word = full_dataset.loc[random_index]
 
-    if model_type == 'phon':
-        # only select word and initial phoneme
-        translation = random_word['translation']
-        phoneme = random_word['initial phoneme']
-        nodes = [translation, phoneme]
-    elif model_type == 'morph':
-        # only select word and morphosyntax
-        translation = random_word['translation']
-        case = random_word['case']
-        plurality = random_word['plurality']
-        definiteness = random_word['definiteness']
-        nodes = [translation, case, plurality, definiteness]
-    elif model_type == "reg":
-        # select all aspects relevant to the model
-        translation = random_word['translation']
-        phoneme = random_word['initial phoneme']
-        case = random_word['case']
-        plurality = random_word['plurality']
-        definiteness = random_word['definiteness']
-        nodes = [translation, phoneme, case, plurality, definiteness]
+    # select all aspects relevant to the model
+    translation = random_word['translation']
+    phoneme = random_word['initial phoneme']
+    case = random_word['case']
+    plurality = random_word['plurality']
+    definiteness = random_word['definiteness']
+    nodes = [translation, phoneme, case, plurality, definiteness]
 
     # dictionary of nodes that correspond to the input
     all_nodes = {"O":0, 'p':1, 'pʲ':2, 'b':3, 'bʲ':4, 'f':5, 'fʲ':6, 'v':7, 'vʲ':8, 'm':9, 'mʲ':10, 't':11, 'tʲ':12, 'd':13, 'dʲ':14, 's':15, 'sʲ':16, 'n':17, 'nʲ':18, 'l':19, 'lʲ':20, 'r':21, 'rʲ':22, 'k':23, 'kʲ':24, 'g':25, 'gʲ':26, 'x':27, 'xʲ':28, 'ɣ':29, 'ɣʲ':30, 'ŋ':31, 'ŋʲ':32, 'h':33, 'NOM':34, 'GEN':35, 'DAT':36, 'SG':37, 'PL':38, 'INDEF':39, 'DEF':40, 'bag':41, 'increase':42, 'beer':43, 'cow':44, 'job':45, 'pepper':46, 'window':47, 'deer':48, 'poem':49, 'channel in strand':50, 'fruit':51, 'language':52, 'fox':53, 'eye':54, 'cat':55, 'fog':56, 'book':57, 'mouse':58, 'star':59, 'seal':60, 'complaint':61, 'dress':62, 'newspaper':63, 'washer (person)':64}
