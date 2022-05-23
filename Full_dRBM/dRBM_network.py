2860ce82d3f45c0bbe22b5ffb56a3b3c12522f98
ray pelupessy <tatum44@outlook.com>
2022-05-24 01:01:16 +0200

    added start of code for testing/training data separation

--- Full_dRBM/dRBM_network.py
+++ Full_dRBM/dRBM_network.py
@@ -9,10 +9,11 @@ import matplotlib.pyplot as plt
 new_nodes_connections = setup_network()
 new_data = setup_data()
 training_steps = 1000
+training_type = 'full' #''
 
 # Initiate training
 for _ in range(training_steps):
-    new_nodes_connections[0] = input_word(new_nodes_connections[0], new_data)
+    new_nodes_connections[0] = input_word(new_nodes_connections[0], new_data, training_type)
     new_nodes_connections = one_learning_step(new_nodes_connections[0], new_nodes_connections[1])
 
 # show graph
--- Full_dRBM/setup_input/irish_input.py
+++ Full_dRBM/setup_input/irish_input.py
@@ -10,33 +10,42 @@ def setup_data():
         
         Returns:
         --------
-        input_df: a dataframe of all the data
+        A tuple containing two dataframes
+        train_df: a dataframe of data only used for training
+        test_df: a dataframe of data used for both testing and training
         
     """
     # read input from csv file
     with open("input_data.csv") as f:
-        input_df = pd.read_csv(f, usecols=['translation', 'initial phoneme', 'case', 'plurality', 'definiteness'])
+        train_df = pd.read_csv(f, usecols=['translation', 'initial phoneme', 'case', 'plurality', 'definiteness'])
     
-    return input_df
+    with open("test_data.csv") as f:
+        test_df = pd.read_csv(f, usecols=['translation', 'initial phoneme', 'case', 'plurality', 'definiteness'])
+    
+    return (train_df, test_df)
 
-def input_word(new_nodes, full_dataset):
+def input_word(new_nodes, full_dataset, training_type):
     """ This function chooses a random input word and changes the 
         activations on the input layer accordingly
         
         Parameters:
         ----------
         new_nodes: an array containing the node activations and biases 
-        model_type: a string stating the type of model
-        full_dataset: a dataframe of all words and their characteristics
+        training_type: a string to indicate what data is to be used in training vs testing
+        full_dataset: a tuple with two dataframes of all words and their characteristics
         
         Returns:
         --------
         new_nodes: an array containing the node activations and biases 
         
     """
-    
-    random_index = random.choice(full_dataset.index)
-    random_word = full_dataset.loc[random_index]
+    train_data = full_dataset[0]
+    test_data = full_dataset[1]
+
+    if training_type == 'full':
+        full_dataset = train_data.append(test_data, ignore_index=True)
+        random_index = random.choice(full_dataset.index)
+        random_word = full_dataset.loc[random_index]
 
     # select all aspects relevant to the model
     translation = random_word['translation']
@@ -47,8 +56,8 @@ def input_word(new_nodes, full_dataset):
     nodes = [translation, phoneme, case, plurality, definiteness]
 
     # dictionary of nodes that correspond to the input
-    all_nodes = {"O":0, 'p':1, 'pʲ':2, 'b':3, 'bʲ':4, 'f':5, 'fʲ':6, 'v':7, 'vʲ':8, 'm':9, 'mʲ':10, 't':11, 'tʲ':12, 'd':13, 'dʲ':14, 's':15, 'sʲ':16, 'n':17, 'nʲ':18, 'l':19, 'lʲ':20, 'r':21, 'rʲ':22, 'k':23, 'kʲ':24, 'g':25, 'gʲ':26, 'x':27, 'xʲ':28, 'ɣ':29, 'ɣʲ':30, 'ŋ':31, 'ŋʲ':32, 'h':33, 'dog':34, 'deceiver':35, 'stud/boss':36, 'May (month)':37, 'child':38, 'petal':39, 'knot':40, 'bag':41, 'increase':42, 'beer':43, 'cow':44, 'job':45, 'pepper':46, 'window':47, 'deer':48, 'poem':49, 'channel in strand':50, 'fruit':51, 'language':52, 'fox':53, 'eye':54, 'cat':55, 'fog':56, 'book':57, 'mouse':58, 'star':59, 'seal':60, 'complaint':61, 'dress':62, 'newspaper':63, 'washer (person)':64, 'butterfly':65, 'student':66, 'god':67, 'tavern':68, 'country':69, 'November':70, 'shop':71, 'friend':72, 'license':73, 'hand':74, 'lighter':75, 'bicycle':76, 'spasm':77, 'calf':78, 'generator':79, 'tadition':80, 'nervous system':81, 'NOM':82, 'GEN':83, 'DAT':84, 'SG':85, 'PL':86, 'INDEF':87, 'DEF':88}
-    
+    all_nodes = {"O":0, 'p':1, 'pʲ':2, 'b':3, 'bʲ':4, 'f':5, 'fʲ':6, 'v':7, 'vʲ':8, 'm':9, 'mʲ':10, 't':11, 'tʲ':12, 'd':13, 'dʲ':14, 's':15, 'sʲ':16, 'n':17, 'nʲ':18, 'l':19, 'lʲ':20, 'r':21, 'rʲ':22, 'k':23, 'kʲ':24, 'g':25, 'gʲ':26, 'x':27, 'xʲ':28, 'ɣ':29, 'ɣʲ':30, 'ŋ':31, 'ŋʲ':32, 'h':33, 'dog':34, 'deceiver':35, 'stud/boss':36, 'May (month)':37, 'child':38, 'petal':39, 'knot':40, 'bag':41, 'increase':42, 'beer':43, 'cow':44, 'job':45, 'pepper':46, 'window':47, 'deer':48, 'poem':49, 'channel in strand':50, 'fruit':51, 'language':52, 'fox':53, 'eye':54, 'cat':55, 'fog':56, 'book':57, 'mouse':58, 'star':59, 'seal':60, 'complaint':61, 'dress':62, 'newspaper':63, 'washer (person)':64, 'butterfly':65, 'student':66, 'god':67, 'tavern':68, 'country':69, 'November':70, 'shop':71, 'friend':72, 'license':73, 'hand':74, 'lighter':75, 'bicycle':76, 'spasm':77, 'calf':78, 'generator':79, 'tradition':80, 'nervous system':81, 'NOM':82, 'GEN':83, 'DAT':84, 'SG':85, 'PL':86, 'INDEF':87, 'DEF':88}
+
     # np array with new activation values
     changed_nodes = np.full((89,), -0.5)
 
